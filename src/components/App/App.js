d2a90b98437de8af994f0c66fa784d49a714d8c5
Myron <dubtwee@gmail.com>
2020-02-18 13:18:51 -0600

    alter App BNanner to be a functional component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -15,7 +15,7 @@ class App extends Component {
     render() {
         return (
             <div>
-                <AppBanner hdg="Functional Components" />
+                <AppBanner hdg="Functional Components" subHdg="Yay Subs!!!"/>
                 
                 <Container>
                     <Grid container spacing={3}>
--- src/components/AppBanner/AppBanner.js
+++ src/components/AppBanner/AppBanner.js
@@ -1,15 +1,15 @@
-import React, { Component } from 'react';
+// removed { Component} import
+import React from 'react';
 import logoSvg from './logo.svg';
 
-class AppBanner extends Component {
-    render() {
-        return (
-            <header className="appBanner">
-                <img className="appLogo" src={logoSvg} alt="React Logo"/>
-                <h1>{this.props.hdg}</h1>
-            </header>
-        );
-    }
+function AppBanner(props) {
+    return (
+        <header className="appBanner">
+            <img className="appLogo" src={logoSvg} alt="React Logo"/>
+            <h1>{props.hdg}</h1>
+            <p>{props.subHdg}</p>
+        </header>
+    );
 }
 
 export default AppBanner;
